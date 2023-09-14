355b4bfc35bb90b0fdcb758f7a62a4115e0f9b29
rose1p <140360518+rose1p@users.noreply.github.com>
2023-09-14 19:16:38 +0900

    CSS 기초파일 class02

---
+++ src/cycle.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+export function Sycle() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p> {count} 번 클릭했습니다. </p>
+            <button onClick={() => setCount(count + 1)}>
+                Click!
+            </button>
+        </div>
+    );
+}
